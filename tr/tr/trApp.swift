15e7b08b93bd28dcb32aa1de38b95e833714461c
Baiel <abdulinbayel1705@gmail.com>
2024-11-15 11:30:01 +0600

    playground and project for mac

---
+++ tr/tr/trApp.swift
@@ -0,0 +1,17 @@
+//
+//  trApp.swift
+//  tr
+//
+//  Created by Абдулин Байэль on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct trApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
